3238c02b4d95718565c7856e7c4ea4214de80c8c
Jonny-Borges <123957006+Jonny-Borges@users.noreply.github.com>
2024-12-11 10:44:18 -0300

    subindo estrutura junto com login e rota login

--- easy_to_move/src/app/app.routes.ts
+++ easy_to_move/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './core/auth/componentes/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent }, // Rota para a tela de login
+    { path: '', redirectTo: '/login', pathMatch: 'full' }, // Redireciona rota raiz para login
+    { path: '**', redirectTo: '/login' } // Rota fallback para login
+];
