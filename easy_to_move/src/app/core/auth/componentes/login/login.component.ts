70f53f4ee6aaf66635cac31cc63cb287ed8744b3
Jonny-Borges <123957006+Jonny-Borges@users.noreply.github.com>
2024-12-12 10:42:16 -0300

    tela login html

--- easy_to_move/src/app/core/auth/componentes/login/login.component.ts
+++ easy_to_move/src/app/core/auth/componentes/login/login.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
-  templateUrl: './login.component.html',
+  imports: [
+    FormsModule 
+  ],
+  templateUrl: './login.component.html',  
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  email: string = '';
+  password: string = '';
+
+  // Método para tratar o login
+  login() {
+    console.log('Email:', this.email);
+    console.log('Senha:', this.password);
+    // Aqui futuramente você chamará o serviço para autenticação
+  }
 
 }
