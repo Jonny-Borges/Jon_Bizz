413e535c907773e2e5698857d1f4c7bce34132a5
Jonny-Borges <123957006+Jonny-Borges@users.noreply.github.com>
2024-12-13 09:26:08 -0300

    pagina de estoque

--- easy_to_move/src/app/app.routes.ts
+++ easy_to_move/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './core/auth/componentes/login/login.component';
 import { MainLayoutComponent } from './layout/main-layout.component';
 import { HomeComponent } from './features/home/home.component';
+import { EstoqueComponent } from './features/estoque/estoque.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -11,10 +12,12 @@ export const routes: Routes = [
     children: [
       { path: '', redirectTo: '/home', pathMatch: 'full' },  // Redirecionamento para Home
       { path: 'home', component: HomeComponent },           // Página inicial
+      { path: 'estoque',component: EstoqueComponent },
       // Adicione outras rotas filhas conforme necessário:
       // { path: 'estoque', component: EstoqueComponent },
       // { path: 'produtos', component: ProdutosComponent },
     ],
   },
   { path: '**', redirectTo: '/login' },  // Redireciona qualquer caminho inválido para login
+  { path: 'estoque',component: EstoqueComponent },
 ];
