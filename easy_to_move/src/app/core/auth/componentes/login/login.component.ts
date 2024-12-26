e5778d0ab731579b7480027601bd790fdc70920f
Jonny-Borges <123957006+Jonny-Borges@users.noreply.github.com>
2024-12-26 14:16:40 -0300

    ajuste no login e direcionamento de rota

--- easy_to_move/src/app/core/auth/componentes/login/login.component.ts
+++ easy_to_move/src/app/core/auth/componentes/login/login.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../auth.service';
 
 
 @Component({
@@ -11,22 +13,23 @@ import { FormsModule } from '@angular/forms';
     CommonModule 
   ],
   templateUrl: './login.component.html',  
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss'],
 })
+
 export class LoginComponent {
-  
   email: string = '';
   password: string = '';
   errorMessage: string = '';
 
-  login() {
+  constructor(private authService: AuthService, private router: Router) {}
 
+  login() {
     if (this.email === 'admin@teste.com' && this.password === '123456') {
       console.log('Login bem-sucedido!');
       this.errorMessage = '';
+      this.router.navigate(['/home']); // Redireciona para a rota "/home"
     } else {
       this.errorMessage = 'Email ou senha inválidos.';
     }
-
   }
 }
