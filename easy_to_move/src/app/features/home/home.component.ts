186b2a204d032a1e215722a8a74d97cd55045eb3
Jonny-Borges <jonnyborges@hotmail.com>
2025-01-03 06:52:03 -0300

    adicao de chart.js e ajuste na tela de home

--- easy_to_move/src/app/features/home/home.component.ts
+++ easy_to_move/src/app/features/home/home.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  cards = [
+    { titulo: 'Clientes', descricao: 'Gerencie seus clientes cadastrados.', botao: 'Acessar', link: '/clientes' },
+    { titulo: 'Fornecedores', descricao: 'Controle seus fornecedores.', botao: 'Acessar', link: '/fornecedor' },
+    { titulo: 'Estoque', descricao: 'Gerencie seu estoque.', botao: 'Acessar', link: '/estoque' },
+    { titulo: 'Vendas', descricao: 'Acompanhe suas vendas.', botao: 'Acessar', link: '/vendas' },
+    { titulo: 'Alertas', descricao: 'Produtos com estoque baixo.', botao: 'Ver Alertas', link: '/estoque' },
+    { titulo: 'Configurações', descricao: 'Ajuste as preferências.', botao: 'Configurar', link: '/configuracoes' },
+  ];
+
 
 }
